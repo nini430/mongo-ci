78caf4b001b0bee7742f554832a459a6a1196ea8
nini430 <105129369+nini430@users.noreply.github.com>
2026-05-04 09:31:46 +0400

    update code

--- data/database.js
+++ data/database.js
@@ -5,10 +5,11 @@ const dbUser = process.env.MONGODB_USERNAME;
 const dbPassword = process.env.MONGODB_PASSWORD;
 const dbName = process.env.MONGODB_DB_NAME;
 
-const uri = `mongodb+srv://${dbUser}:${dbPassword}@${clusterAddress}/?retryWrites=true&w=majority`;
-let client = new MongoClient(uri);
+//const uri = `mongodb+srv://${dbUser}:${dbPassword}@${clusterAddress}/?retryWrites=true&w=majority`;
+const uri = `mongodb://${dbUser}:${dbPassword}@localhost:27017/${dbName}authSource=admin&directConnection=true&serverSelectionTimeoutMS=5000`;
 
-
+const client = new MongoClient(uri);
+let database;
 
 
 
@@ -17,13 +18,12 @@ try {
   console.log('Hang On! Connecting to your DB...');
   await client.connect();
   console.log('Connected to your DB Server');
-  const database = client.db(dbName);
+  database = client.db(dbName);
   await database.command({ ping: 1 });
 
   console.log('Ping Successful!');
 
 } catch (error) {
-  console.log(error);
   console.log('Oops! Connection failed.');
   process.exit(1);
 }
